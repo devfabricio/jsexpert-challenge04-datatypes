6e2e5ba1a92b89d8f129d001aa4a37927902a824
Fabrício Augusto <fabricio.mata@gmail.com>
2022-03-09 18:53:43 -0300

    feat: modify toString method to to be displayed correctly in the terminal

--- src/entity/User.js
+++ src/entity/User.js
@@ -2,7 +2,9 @@ class User {
   constructor({ id }) {
     this.id = id;
   }
-  // TODO: Alguma manipulação a fazer considerando que o objeto gerado será constantemente escrito em tela?
+  toString() {
+    return `[${this.id}]`
+  }
 }
 
 export default User;
